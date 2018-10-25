5e33d4d3d62932f7e85856d6d7d0ecab245327ed
marvined77 <44221160+marvined77@users.noreply.github.com>
2018-10-25 09:56:21 -0600

    Add files via upload

---
+++ app/src/main/java/sv/edu/catolica/vlsp_inventarios/VentasFin.java
@@ -0,0 +1,18 @@
+package sv.edu.catolica.vlsp_inventarios;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class VentasFin extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ventas_fin, container, false);
+    }
+
+}
