ffc043a92af637a5ac9c45bd58b278ec6981be3a
marvined77 <44221160+marvined77@users.noreply.github.com>
2018-11-05 11:01:53 -0600

    ya agrega prod, solo falta la imagen de producto

--- app/src/main/java/sv/edu/catolica/vlsp_inventarios/AgregarProductos.java
+++ app/src/main/java/sv/edu/catolica/vlsp_inventarios/AgregarProductos.java
@@ -1,6 +1,6 @@
 package sv.edu.catolica.vlsp_inventarios;
 
-import android.content.Intent;
+import android.*;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.annotation.Nullable;
@@ -9,10 +9,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -20,9 +24,34 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import Clases.Categoria;
+import Clases.Producto;
+
 public class AgregarProductos extends Fragment {
+    ConnectionClass con = new ConnectionClass();
+    Connection cn = con.CONN();
+
+    EditText NombreP, DescripP, CantP, CostoP, PrecioVentaP,FechaVencP;
     Spinner ListaCategorias;
-    ArrayList<String> values;
+    Button Add, BtnImg;
+    String Nombre, Descrip, FechaV;
+    float Stock;
+    Double Costo, PrecioV;
+
+    public List<String> ListS(){
+        List<String> values = new ArrayList<>();
+        try{
+            String query="SELECT * FROM CATEGORIA WHERE idEmpresa=1";
+            Statement st=cn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()){
+                values.add(rs.getInt("idCat") + " - " + rs.getString("cat_name"));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return values;
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -35,28 +64,43 @@ public class AgregarProductos extends Fragment {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
+        //------------------------------------------------------------
+        NombreP=(EditText)getView().findViewById(R.id.txtPnombre);
+        DescripP=(EditText)getView().findViewById(R.id.txtPdescrip);
+        CantP=(EditText)getView().findViewById(R.id.txtCant);
+        CostoP=(EditText)getView().findViewById(R.id.txtPcosto);
+        PrecioVentaP=(EditText)getView().findViewById(R.id.txtPprecio);
+        FechaVencP=(EditText)getView().findViewById(R.id.txtPvence);
         ListaCategorias=(Spinner)getView().findViewById(R.id.spnCat);
-        ConnectionClass con = new ConnectionClass();
-        Connection cn = con.CONN();
-        Statement pst;
-        values = new ArrayList<String>();
-
-        try{
-            pst=(Statement)cn.createStatement();
-            String query="SELECT cat_name FROM CATEGORIA WHERE idEmpresa=1";
-            ResultSet rs = pst.executeQuery(query);
-            values.add("Seleccione");
-            while (rs.next()){
-                rs.getInt(0);
-                rs.getInt(1);
-                Toast.makeText(getActivity(), rs.getString(2), Toast.LENGTH_SHORT).show();
-                values.add(rs.getString(2));
+        BtnImg=(Button)getView().findViewById(R.id.imgProd);
+        Add=(Button)getView().findViewById(R.id.btnAggP);
+        //------------------------------------------------------------
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String itemText = (String) ListaCategorias.getSelectedItem();
+                String[] s =  itemText.split("-");
+                String idCat = s[0].trim();
+                Nombre=NombreP.getText().toString();
+                Descrip=DescripP.getText().toString();
+                FechaV=FechaVencP.getText().toString();
+                Stock=Float.parseFloat(CantP.getText().toString());
+                Costo=Double.parseDouble(CostoP.getText().toString());
+                PrecioV=Double.parseDouble(PrecioVentaP.getText().toString());
+                try{
+                    String query = "INSERT INTO PRODUCTO (idEmpresa,idCat,producto_name,producto_stock,producto_price,producto_cost,producto_desc,producto_exp_date) VALUES " +
+                            "(1,"+idCat+",'"+Nombre+"',"+Stock+","+PrecioV+","+Costo+",'"+Descrip+"','"+FechaV+"')";
+                    PreparedStatement pst=cn.prepareStatement(query);
+                    pst.executeUpdate();
+                    Toast.makeText(getActivity(), "Registro agregado con éxito", Toast.LENGTH_SHORT).show();
+                }catch (SQLException e){
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(), "Error al agregar registro", Toast.LENGTH_SHORT).show();
+                }
             }
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item,values);
+        });
+        //------------------------------------------------------------
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item,ListS());
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         ListaCategorias.setAdapter(dataAdapter);
     }
