58605538f51aa950adb06d699b07137b2543f508
petter27 <42980286+petter27@users.noreply.github.com>
2018-11-07 20:52:35 -0600

    Agregando opcion de ventas
    Cambiando interfaz y otros detalles

--- app/src/main/java/sv/edu/catolica/vlsp_inventarios/Ventas.java
+++ app/src/main/java/sv/edu/catolica/vlsp_inventarios/Ventas.java
@@ -14,6 +14,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,6 +43,12 @@ public class Ventas extends Fragment {
     TextView tvnombre,tvprecio,tvdescrip;
     ArrayList<ProductoModel> productoInfo=new ArrayList<>();
     ArrayList<ProductoModel> lsProdVenta=new ArrayList<>();
+
+    TableLayout tbProductos;
+    public double total=0;
+    TextView tvTotal;
+    public Button btnAgg;
+
     public int IndSelected=-1;
 
     public List<String> Obt_Productos(){
@@ -88,22 +96,91 @@ public class Ventas extends Fragment {
         tvdescrip=(TextView) getView().findViewById(R.id.tvdescP);
         tvprecio=(TextView) getView().findViewById(R.id.tvprecioP);
         etCantidad=(EditText) getView().findViewById(R.id.etCantidad);
+
+        btnAgg=(Button) getView().findViewById(R.id.btnAggP);
+        tbProductos=(TableLayout) getView().findViewById(R.id.tblDetalleVentas);
+        tvTotal=(TextView) getView().findViewById(R.id.tvTotal);
         //------------------------------------------------------------
         btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-if (productoInfo.get(IndSelected).producto_stock> Integer.parseInt(etCantidad.getText().toString()) && etCantidad.getText().length()>0){
 
-    //Guardando registro seleccionado en la lista de productos a vender
+         tbProductos.removeAllViews();
+         int suma=0;
+         boolean producoExistente=false;
+         int pd=-1;
+                String itemS= ProductList.getSelectedItem().toString();
 
-    lsProdVenta.add(productoInfo.get(IndSelected));
-                Toast.makeText(getContext(), "Item agregado",Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(getContext(), "Stock Insuficiente",Toast.LENGTH_SHORT).show();
+         for (int j=0; j<lsProdVenta.size();j++) {
+               if (itemS.equals(productoInfo.get(j).producto_name)) {
+                        producoExistente=true;
+                        suma=productoInfo.get(j).cant+Integer.parseInt(etCantidad.getText().toString());
+                        pd=j;
+                    }
                 }
+                if(producoExistente==true && pd>-1){
+                    if(suma<productoInfo.get(pd).producto_stock){
+                        lsProdVenta.get(pd).cant=suma;
+                    }
+         }
+                    if (productoInfo.get(IndSelected).producto_stock > Integer.parseInt(etCantidad.getText().toString()) && etCantidad.getText().length() > 0) {
 
+                        //Guardando registro seleccionado en la lista de productos a vender
+                        if (producoExistente==false){
+                        productoInfo.get(IndSelected).cant = Integer.parseInt(etCantidad.getText().toString());
+                        lsProdVenta.add(productoInfo.get(IndSelected));
+                        }
+                    //jajaja
+                    for (int x = 0; x < lsProdVenta.size(); x++) {
+                        TableRow row = new TableRow(getContext());
+                        int id = lsProdVenta.get(x).idProducto;
+                        String nomb = lsProdVenta.get(x).producto_name;
+                        String desc = lsProdVenta.get(x).producto_desc;
+                        String price = Double.toString(lsProdVenta.get(x).producto_price);
+                        int cant=0;
+                        if(producoExistente==true && lsProdVenta.get(pd).producto_name.equals(lsProdVenta.get(x).producto_name)){
+                        cant=suma;
+                        }else{
+                        cant = lsProdVenta.get(x).cant;
+                        }
+                        TextView tvid = new TextView(getContext());
+                        tvid.setText("" + id);
+                        TextView tvNomb = new TextView(getContext());
+                        tvNomb.setText("" + nomb);
+                        TextView tvPrice = new TextView(getContext());
+                        tvPrice.setText(" $" + price);
+                        TextView tvCant = new TextView(getContext());
+                        tvCant.setText("" + cant);
+
+                        row.addView(tvid);
+                        row.addView(tvNomb);
+                        row.addView(tvPrice);
+                        row.addView(tvCant);
+                        tbProductos.addView(row);
+
+                        total = total + (lsProdVenta.get(x).producto_price * cant);
+                    }
+                    tvTotal.setText("Total de la venta: $" + total);
+                    producoExistente=false;
+                    total=0;
+    Toast.makeText(getContext(), "Item agregado",Toast.LENGTH_SHORT).show();
+    }else{
+    Toast.makeText(getContext(), "Stock Insuficiente",Toast.LENGTH_SHORT).show();
+}
+
+//aqui va el fucking corchete
             }
         });
+
+    btnAgg.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getContext(),"venta realizada",Toast.LENGTH_SHORT).show();
+        }
+    });
+
+    //jajaja
+
 
         btnDetalle.setOnClickListener(new View.OnClickListener() {
             @Override
